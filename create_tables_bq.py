b246f2ca2ac4b0a165b93e6eedfb727d263c7011
acanavero <122903422+acanavero@users.noreply.github.com>
2023-04-23 20:40:02 -0300

    tables are now created with required fields

--- create_tables_bq.py
+++ create_tables_bq.py
@@ -2,6 +2,7 @@ from google.cloud import bigquery
 from google.oauth2 import service_account
 import pandas as pd
 from utils import PROJECT_ID, TABLE_ID_EMPLOYEES, TABLE_ID_DEPARTMENTS, TABLE_ID_JOBS, CREDENTIALS_PATH
+from utils import hired_employees_schema, departments_schema, jobs_schema
 
 if __name__ == '__main__':
     
@@ -16,23 +17,24 @@ if __name__ == '__main__':
 
         hired_employees =  pd.read_csv('./initial_data/hired_employees.csv')
 
-        jobs = pd.read_csv('./initial_data/jobs.csv')
+        jobs = pd.read_csv('./initial_data/jobs.csv', dtype={"id":'int32', "job": "string"})
 
-        departments = pd.read_csv('./initial_data/departments.csv')
+        departments = pd.read_csv('./initial_data/departments.csv', dtype={"id":'int32', "department": "string"})
 
         hired_employees.columns = ['id','name','datetime', 'department_id', 'job_id']
+        hired_employees['name'] = hired_employees['name'].astype('str')
         departments.columns = ['id', 'department']
         jobs.columns = ['id', 'job']
-        print( hired_employees)
+        print(hired_employees)
         print(jobs)
         print(departments)
         
         print("Uploading hired_employees!")
-        hired_employees.to_gbq(project_id=PROJECT_ID, destination_table=TABLE_ID_EMPLOYEES, progress_bar= True, credentials=credentials, if_exists='replace')
+        hired_employees.to_gbq(project_id=PROJECT_ID, destination_table=TABLE_ID_EMPLOYEES, progress_bar= True, credentials=credentials, if_exists='replace', table_schema=hired_employees_schema)
         print("Uploading jobs!")
-        jobs.to_gbq(project_id=PROJECT_ID, destination_table=TABLE_ID_JOBS, progress_bar= True, credentials=credentials, if_exists='replace')
+        jobs.to_gbq(project_id=PROJECT_ID, destination_table=TABLE_ID_JOBS, progress_bar= True, credentials=credentials, if_exists='replace', table_schema= jobs_schema)
         print("Uploading departments!")
-        departments.to_gbq(project_id=PROJECT_ID, destination_table=TABLE_ID_DEPARTMENTS, progress_bar= True, credentials=credentials, if_exists='replace')
+        departments.to_gbq(project_id=PROJECT_ID, destination_table=TABLE_ID_DEPARTMENTS, progress_bar= True, credentials=credentials, if_exists='replace', table_schema= departments_schema)
 
         print("Upload Succesful")
     else:
